54f0a867c421b4a5e61b25a2220ee3db61b411b2
Vinicius Zuchi <liquimaster@gmail.com>
2023-07-06 21:04:18 +0200

    remove testing stuff from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Link from 'next/link';
-import { Test } from '@core-ui/Test';
 
 import styles from './page.module.css';
 
 const Home = () => {
     return (
         <main className={styles.main}>
-            <Test />
-            <i className="fas fa-home"></i>
             <Link href="/privacy_policy">Privacy policy</Link>
         </main>
     );
