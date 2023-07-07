4addf270703c5e36bee8dcce830c6b44591b0d74
Vinicius Zuchi <liquimaster@gmail.com>
2023-07-07 21:42:00 +0200

    try getting the graphql endpoint always from process.env

--- packages/urql-utils/get-gql-host.ts
+++ packages/urql-utils/get-gql-host.ts
@@ -1,15 +1,7 @@
 import getConfig from 'next/config';
 
-import { isDev } from '../utils';
-
 const { serverRuntimeConfig } = getConfig() || {};
 
 export const getGraphqlHost = (): string => {
-    if (isDev()) {
-        return process.env.GRAPHQL_ENDPOINT || '/graphql';
-    }
-
-    console.log(serverRuntimeConfig);
-
-    return serverRuntimeConfig?.GRAPHQL_ENDPOINT || '/graphql';
+    return process.env.GRAPHQL_ENDPOINT || '/graphql';
 };
